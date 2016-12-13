ab528ca6ac877c440db0b26fd166995510a75f88
Matt Collins <matt@strikesandgutters.com>
2016-12-13 13:49:34 +0000

    remove old images

--- train.sh
+++ train.sh
@@ -3,7 +3,7 @@ if [ -z "$APIKEY" ]; then
   echo "Please set APIKEY environment variable"
   exit 1
 fi
-curl -X POST -F "clubs_positive_examples=@clubs.zip" -F "spades_positive_examples=@spades.zip" -F "name=cards" "https://gateway-a.watsonplatform.net/visual-recognition/api/v3/classifiers?api_key=$APIKEY&version=2016-05-20"
+curl -X POST -F "clubs_positive_examples=@clubs.zip" -F "diamonds_positive_examples=@diamonds.zip" -F "name=cards" "https://gateway-a.watsonplatform.net/visual-recognition/api/v3/classifiers?api_key=$APIKEY&version=2016-05-20"
 
 
-# curl -X GET "https://gateway-a.watsonplatform.net/visual-recognition/api/v3/classifiers/cards_1415156717?api_key=$APIKEY&version=2016-05-20"
+# curl -X GET "https://gateway-a.watsonplatform.net/visual-recognition/api/v3/classifiers/cards_1844303126?api_key=$APIKEY&version=2016-05-20"
